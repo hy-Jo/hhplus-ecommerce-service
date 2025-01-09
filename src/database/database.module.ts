4c4767afac322e346a3d4d48fe4640be0ea78bd8
hy-jo <jo.hy2503@gmail.com>
2025-01-09 23:41:09 +0900

    chnage typeORM to Prisma

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,28 +1,14 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { DatabaseConfig, dbConfig } from "./database.config";
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [
-        ConfigModule.forRoot({
-          load: [dbConfig],
-          envFilePath: `.env.${process.env.NODE_ENV}`,
-        }),
-      ],
-      useFactory: (configService: ConfigService) => ({
-        type: "mysql",
-        ...configService.get<DatabaseConfig>("database"),
-        synchronize: false,
-        autoLoadEntities: true,
-        relationLoadStrategy: "join",
-      }),
-      inject: [ConfigService],
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
   ],
-  controllers: [],
-  providers: [],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class DatabaseModule {}
