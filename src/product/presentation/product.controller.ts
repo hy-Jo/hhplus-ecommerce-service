7a27f4966bf56ff3208e76c6bc1b8006c17c3c57
hy-jo <jo.hy2503@gmail.com>
2025-01-10 02:08:38 +0900

    feat: add product

--- src/product/presentation/product.controller.ts
+++ src/product/presentation/product.controller.ts
@@ -1,17 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ProductService } from '../application/product.service';
 import { ProductDto } from '../domain/dto/product.dto';
 
 @ApiTags('products')
 @Controller('products')
 export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
   @Get('/')
   @ApiOperation({ summary: '상품 목록 조회' })
   @ApiResponse({ type: [ProductDto] })
-  getProducts() {}
+  async getProducts(@Query('name') name?: string): Promise<ProductDto[]> {
+    return this.productService.getProducts(name);
+  }
 
-  @Get('/popular')
-  @ApiOperation({ summary: '인기 상품 조회' })
-  @ApiResponse({ type: [ProductDto] })
-  getPopularProducts() {}
+  // @Get('/popular')
+  // @ApiOperation({ summary: '인기 상품 조회' })
+  // @ApiResponse({ type: [ProductDto] })
+  // async getPopularProducts(): Promise<ProductDto[]> {
+  //   return this.productService.getPopularProducts();
+  // }
+
+  @Get('/:id')
+  @ApiOperation({ summary: '상품 조회' })
+  @ApiResponse({ type: ProductDto })
+  async getProduct(@Param('id') id: number): Promise<ProductDto> {
+    return this.productService.getProductById(id);
+  }
 }
